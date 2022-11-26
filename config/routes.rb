f09c2d4b64b661c610e7c56a249aa192710ba172
teejaydixon <tahjgdixon@gmail.com>
2022-11-26 10:32:12 -0800

    render config

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,11 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-  namespace :api do
-  # Defines the root path route ("/")
+ 
   # root "articles#index"
   # resources :sales
   resources :sneakers, only: [:index, :show]
   resources :users
   # resources :wishitems
   
- 
-
 post '/login', to: 'sessions#create'  
 get '/me', to: 'users#show'
 delete '/logout', to: 'sessions#destroy'
@@ -21,13 +17,6 @@ post '/wishitems', to: 'wish_items#fav_shoe'
 get '/wishitems', to: 'wish_items#index'
 get '/sales', to: 'sales#index'
 post '/sales', to: 'sales#sale_shoe'
-end
-
-
-
-
-
-
 
   get "*path", to: "fallback#index", constraints: ->(req) { !req.xhr? && req.format.html? }
 end
