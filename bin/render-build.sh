f19dd06543a072d82be71e6a5749ac7b9d2f4279
Teejay Dixon <103458131+TeejayDixon@users.noreply.github.com>
2023-03-21 21:05:05 -0700

    Update render-build.sh

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -9,6 +9,7 @@ cp -a client/build/. public/
 
 #Build commands for back end
 bundle install
-bundle exec rake db:migrate VERSION=0
-bundle exec rake db:migrate
-bundle exec rake db:seed
+bundle exec rails assets:precompile
+bundle exec rails assets:clean
+bundle exec rails db:migrate
+bundle exec rails db:seed
